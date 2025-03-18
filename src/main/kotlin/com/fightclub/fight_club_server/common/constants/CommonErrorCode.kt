b4d0f323fe6f1780e05d8295986dd383e32d3339
hwanfront <kim015jh@gmail.com>
2025-03-19 01:28:08 +0900

    feat: 공통 응답코드 추가

---
+++ src/main/kotlin/com/fightclub/fight_club_server/common/constants/CommonErrorCode.kt
@@ -0,0 +1,15 @@
+package com.fightclub.fight_club_server.common.constants
+
+import org.springframework.http.HttpStatus
+
+enum class CommonErrorCode(
+    override val status: HttpStatus,
+    override val code: String,
+    override val message: String
+): ResponseCode {
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "CE001", "잘못된 요청입니다."),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "CE002", "인증이 필요합니다."),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "CE003", "접근이 거부되었습니다."),
+    NOT_FOUND(HttpStatus.NOT_FOUND, "CE004", "리소스를 찾을 수 없습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "CE005", "서버 내부 오류가 발생했습니다.")
+}
