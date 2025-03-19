2c3e65048e297960f591de75529827139d65e2d5
hwanfront <kim015jh@gmail.com>
2025-03-19 15:17:10 +0900

    docs: 주석 추가

--- src/main/kotlin/com/fightclub/fight_club_server/common/exception/BaseException.kt
+++ src/main/kotlin/com/fightclub/fight_club_server/common/exception/BaseException.kt
@@ -2,6 +2,11 @@ package com.fightclub.fight_club_server.common.exception
 
 import com.fightclub.fight_club_server.common.constants.ResponseCode
 
+// kotlin 에서 기본적으로 클래스는 상속불가 final
+// -> open 키워드를 통해 상속 가능하게 만듦
+// java 는 모든 클래스가 기본적으로 상속 가능하지만
+// java 의 abstract class 는 무조건 상속 해야하는 반면에
+// open class 는 선택적으로 상속 가능함
 open class BaseException(
     private val responseCode: ResponseCode
 ) : RuntimeException(responseCode.message) {
