4e2adedec44a0edbcc4ccc55c748854709c37ab9
hwanfront <kim015jh@gmail.com>
2025-03-19 22:10:12 +0900

    feat: 유효성 검사를 위한 설정 추가

--- src/main/kotlin/com/fightclub/fight_club_server/common/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/fightclub/fight_club_server/common/exception/GlobalExceptionHandler.kt
@@ -1,7 +1,9 @@
 package com.fightclub.fight_club_server.common.exception
 
+import com.fightclub.fight_club_server.common.constants.CommonErrorCode
 import com.fightclub.fight_club_server.common.dto.ApiResponse
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -10,5 +12,11 @@ class GlobalExceptionHandler {
     @ExceptionHandler(BaseException::class)
     fun handleBaseException(e: BaseException): ResponseEntity<ApiResponse<Nothing>> {
         return ApiResponse.error(e.errorCode)
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): ResponseEntity<ApiResponse<Map<String, String>>> {
+        val errorMessages = e.bindingResult.fieldErrors.associate { it.field to (it.defaultMessage ?: "잘못된 값입니다.") }
+        return ApiResponse.error(CommonErrorCode.BAD_REQUEST, errorMessages)
     }
 }
