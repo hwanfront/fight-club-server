4ac8d39f6ec0d1a7f6531683b82a5eab25ab4c00
hwanfront <kim015jh@gmail.com>
2025-03-29 23:54:54 +0900

    refactor: User 엔티티 권한 검증 수정

--- src/main/kotlin/com/fightclub/fight_club_server/user/controller/UserController.kt
+++ src/main/kotlin/com/fightclub/fight_club_server/user/controller/UserController.kt
@@ -2,27 +2,41 @@ package com.fightclub.fight_club_server.user.controller
 
 import com.fightclub.fight_club_server.user.constants.UserSuccessCode
 import com.fightclub.fight_club_server.common.dto.ApiResponse
+import com.fightclub.fight_club_server.user.domain.User
 import com.fightclub.fight_club_server.user.dto.OAuth2SignupRequest
 import com.fightclub.fight_club_server.user.dto.SignupRequest
 import com.fightclub.fight_club_server.user.service.UserService
 import jakarta.validation.Valid
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/users")
 class UserController(private val userService: UserService) {
-    @GetMapping("/me")
-    fun myInfo() = ApiResponse.success(UserSuccessCode.MY_INFO_SUCCESS, userService.myInfo())
 
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/me")
+    fun myInfo(@AuthenticationPrincipal user: User) = ApiResponse.success(UserSuccessCode.MY_INFO_SUCCESS, userService.myInfo(user))
+
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/{userId}")
     fun userInfo(@PathVariable userId: Long) = ApiResponse.success(UserSuccessCode.USER_INFO_SUCCESS, userService.userInfo(userId))
 
+    @PreAuthorize("isAnonymous()")
     @PostMapping("/signup")
     fun signup(@Valid @RequestBody signupRequest: SignupRequest) = ApiResponse.success(UserSuccessCode.SIGNUP_SUCCESS, userService.signup(signupRequest))
 
+    @PreAuthorize("isAuthenticated()")
     @PatchMapping("/complete")
-    fun oAuth2Signup(@Valid @RequestBody oAuth2SignupRequest: OAuth2SignupRequest) = ApiResponse.success(UserSuccessCode.SIGNUP_SUCCESS, userService.oAuth2Signup(oAuth2SignupRequest))
+    fun oAuth2Signup(
+        @AuthenticationPrincipal user: User,
+        @Valid @RequestBody oAuth2SignupRequest: OAuth2SignupRequest
+    ) = ApiResponse.success(UserSuccessCode.SIGNUP_SUCCESS, userService.oAuth2Signup(user, oAuth2SignupRequest))
 
+    @PreAuthorize("isAuthenticated()")
     @DeleteMapping("/delete")
-    fun deleteUser() = ApiResponse.success(UserSuccessCode.DELETE_SUCCESS, userService.deleteUser())
+    fun deleteUser(
+        @AuthenticationPrincipal user: User
+    ) = ApiResponse.success(UserSuccessCode.DELETE_SUCCESS, userService.deleteUser(user))
 }
