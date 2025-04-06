065b5dd2f0afd17d78edcf294b67c1ce2fd90358
hwanfront <65995108+hwanfront@users.noreply.github.com>
2025-04-06 18:09:11 +0900

    test: failed test case

--- src/test/kotlin/com/fightclub/fight_club_server/FightClubServerApplicationTests.kt
+++ src/test/kotlin/com/fightclub/fight_club_server/FightClubServerApplicationTests.kt
@@ -4,5 +4,10 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class FightClubServerApplicationTests {
+class FightClubServerApplicationTests {	
+
+	@Test
+	fun failTest() {
+		fail("의도적으로 실패하는 테스트입니다.")
+	}
 }
