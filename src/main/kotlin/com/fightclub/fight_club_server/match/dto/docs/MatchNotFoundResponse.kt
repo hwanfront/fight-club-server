d0fc6182ec2bf306b9fea449ada32bb8763336f8
hwanfront <kim015jh@gmail.com>
2025-04-19 22:18:48 +0900

    docs: swagger 문서 수정

--- src/main/kotlin/com/fightclub/fight_club_server/match/dto/docs/MatchNotFoundResponse.kt
+++ src/main/kotlin/com/fightclub/fight_club_server/match/dto/docs/MatchNotFoundResponse.kt
@@ -4,15 +4,15 @@ import com.fightclub.fight_club_server.common.dto.ApiResult
 import com.fightclub.fight_club_server.match.constants.MatchConstants
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(description = "매치 정보 조회 실패")
+@Schema(description = "매치를 찾을 수 없음")
 data class MatchNotFoundResponse(
-    @Schema(description = "HTTP 상태 코드", example = MatchConstants.GetMatchInfoSuccess.STATUS_CODE)
+    @Schema(description = "HTTP 상태 코드", example = MatchConstants.MatchNotFound.STATUS_CODE)
     override val status: Int,
 
-    @Schema(description = "응답 코드", example = MatchConstants.GetMatchInfoSuccess.CODE)
+    @Schema(description = "응답 코드", example = MatchConstants.MatchNotFound.CODE)
     override val code: String,
 
-    @Schema(description = "응답 메시지", example = MatchConstants.GetMatchInfoSuccess.MESSAGE)
+    @Schema(description = "응답 메시지", example = MatchConstants.MatchNotFound.MESSAGE)
     override val message: String,
 
     override val data: Nothing
--- src/main/kotlin/com/fightclub/fight_club_server/match/dto/docs/UserIsNotParticipantResponse.kt
+++ src/main/kotlin/com/fightclub/fight_club_server/match/dto/docs/UserIsNotParticipantResponse.kt
@@ -4,15 +4,15 @@ import com.fightclub.fight_club_server.common.dto.ApiResult
 import com.fightclub.fight_club_server.match.constants.MatchConstants
 import io.swagger.v3.oas.annotations.media.Schema
 
-@Schema(description = "매치 정보 조회 실패")
+@Schema(description = "사용자가 매치의 참가자가 아님")
 data class UserIsNotParticipantResponse(
-    @Schema(description = "HTTP 상태 코드", example = MatchConstants.GetMatchInfoSuccess.STATUS_CODE)
+    @Schema(description = "HTTP 상태 코드", example = MatchConstants.UserIsNotParticipant.STATUS_CODE)
     override val status: Int,
 
-    @Schema(description = "응답 코드", example = MatchConstants.GetMatchInfoSuccess.CODE)
+    @Schema(description = "응답 코드", example = MatchConstants.UserIsNotParticipant.CODE)
     override val code: String,
 
-    @Schema(description = "응답 메시지", example = MatchConstants.GetMatchInfoSuccess.MESSAGE)
+    @Schema(description = "응답 메시지", example = MatchConstants.UserIsNotParticipant.MESSAGE)
     override val message: String,
 
     override val data: Nothing
